c7c59e5658afc76f9bf847245372f559bcaf459c
jose_a <josefa.aste@estudiante.uc.cl>
2026-03-26 20:17:41 -0300

    juego

--- GH1.py
+++ GH1.py
@@ -10,3 +10,34 @@ while jugador not in compu_l:
 
 rando = randint(0, 4)
 compu = compu_l[rando]
+
+
+print('\nEl computador tiro', compu,'\n')
+
+if jugador == compu:
+    print("¡Empataron!")
+elif jugador == 'piedra':
+    if compu == 'papel' or compu == 'spock':
+        print('¡Perdiste!', compu, 'vence a', jugador)
+    else:
+        print('¡Ganaste!', jugador, 'vence a', compu)
+elif jugador == 'papel':
+    if compu == 'tijera' or compu == 'lagartija':
+        print('¡Perdiste!', compu, 'vence a', jugador)
+    else:
+        print('¡Ganaste!', jugador, 'vence a', compu)
+elif jugador == 'tijera':
+    if compu == 'spock' or compu == 'piedra':
+        print('¡Perdiste!', compu, 'vence a', jugador)
+    else:
+        print('¡Ganaste!', jugador, 'vence a', compu)
+elif jugador == 'lagartija':
+    if compu == 'tijera' or compu == 'piedra':
+        print('¡Perdiste!', compu, 'vence a', jugador)
+    else:
+        print('¡Ganaste!', jugador, 'vence a', compu)
+else: #spocvk
+    if compu == 'papel' or compu == 'lagartija':
+        print('¡Perdiste!', compu, 'vence a', jugador)
+    else:
+        print('¡Ganaste!', jugador, 'vence a', compu)
--- nuevo.py
+++
@@ -1,3 +0,0 @@
-#miau 
-def pasear():
-    pass
