61a4db1c6de73740612485b03732895d45996cec
jose_a <josefa.aste@estudiante.uc.cl>
2026-03-26 20:41:31 -0300

    finmales

--- GH1.py
+++ GH1.py
@@ -2,7 +2,7 @@
 from random import randint
 print('\nPara jugar, elegir un elemento: piedra, papel, tijera, lagartija, o spock\n')
 
-compu_l = ['piedra','papel', 'tijera','lagartija', 'spock']
+compu_l = ['papel','piedra', 'lagartija','spock','tijera']
 
 papel = ["-", 1, 2, 1, 2]
 
@@ -31,9 +31,9 @@ while jugar == True:
         jugador = input('Eso no es un elemento. Anotar elemento aquí: ').lower()
 
     rando = randint(0, 4)
-    pri = compu_l[rando]
-    jug = compu_l.index(jugador)
-    print('El computador tiro',pri)
+    print('El computador tiro',compu_l[rando])
+    jug = int(compu_l.index(jugador))
+
     print(jugada(jug,rando))
     print("Quieres volver a jugar?")
     print("1. Sí!!")
