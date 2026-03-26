7c9e8794209c4137f26e5400b61468ac75c35056
jose_a <josefa.aste@estudiante.uc.cl>
2026-03-26 16:00:25 -0300

    crear tiradas

--- GH1.py
+++ GH1.py
@@ -1,3 +1,12 @@
 #miau 
-def pasear():
-    pass
+from random import randint
+print('\nPara jugar, elegir un elemento: piedra, papel, tijera, lagartija, o spock\n')
+
+compu_l = ['piedra','papel', 'tijera','lagartija', 'spock']
+
+jugador = input('Anotar elemento aquí: ').lower()
+while jugador not in compu_l:
+    jugador = input('Eso no es un elemento. Anotar elemento aquí: ').lower()
+
+rando = randint(0, 4)
+compu = compu_l[rando]
